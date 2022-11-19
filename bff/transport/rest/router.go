30caaf60a31c3fcab939262bc9d880e84e057709
harryzcy <harry@harryzheng.com>
2022-11-19 02:59:27 -0500

    [bff] Setup bff router and proxy requests

--- bff/transport/rest/router.go
+++ bff/transport/rest/router.go
@@ -1,11 +1,16 @@
 package rest
 
 import (
+	"path/filepath"
+	"strings"
 	"time"
 
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+
+	"github.com/harryzcy/mailbox-browser/bff/config"
+	"github.com/harryzcy/mailbox-browser/bff/transport/rest/web"
 )
 
 func Init(logger *zap.Logger, mode string) *gin.Engine {
@@ -19,6 +24,27 @@ func Init(logger *zap.Logger, mode string) *gin.Engine {
 
 	r := gin.New()
 	r.Use(ginzap.Ginzap(logger, time.RFC3339, true), ginzap.RecoveryWithZap(logger, true))
+
+	webPath := r.Group("/web")
+	{
+		emails := webPath.Group("/emails")
+		emails.GET("", web.MailboxProxy)              // list
+		emails.GET("/:id", web.MailboxProxy)          // get
+		emails.POST("", web.MailboxProxy)             // create
+		emails.POST("/:id", web.MailboxProxy)         // save
+		emails.DELETE("/:id", web.MailboxProxy)       // delete
+		emails.POST("/:id/trash", web.MailboxProxy)   // trash
+		emails.POST("/:id/untrash", web.MailboxProxy) // untrash
+		emails.POST("/:id/send", web.MailboxProxy)    // send
+	}
+
+	r.NoRoute(func(c *gin.Context) {
+		if (c.Request.URL.Path == "/") || (c.Request.URL.Path == "/index.html") {
+			c.File(config.INDEX_HTML)
+		} else if strings.HasPrefix(c.Request.URL.Path, "/assets/") {
+			c.File(filepath.Join(config.STATIC_DIR, c.Request.URL.Path))
+		}
+	})
 
 	return r
 }
---
+++ bff/transport/rest/web/proxy.go
@@ -0,0 +1,36 @@
+package web
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/harryzcy/mailbox-browser/bff/config"
+)
+
+func MailboxProxy(ctx *gin.Context) {
+	method := ctx.Request.Method
+	url := config.MAILBOX_URL + ctx.Request.URL.Path
+
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	req, err := http.NewRequest(method, url, ctx.Request.Body)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	defer resp.Body.Close()
+	ctx.DataFromReader(http.StatusOK, resp.ContentLength, resp.Header.Get("Content-Type"), resp.Body, nil)
+}
