e48ea945f4121dcaed55dace361f4912ad3ca6ec
harryzcy <harry@harryzheng.com>
2022-12-19 22:50:32 -0500

    Load aws access key from env variables

--- bff/config/config.go
+++ bff/config/config.go
@@ -9,5 +9,7 @@ var (
 	STATIC_DIR = os.Getenv("STATIC_DIR")
 	INDEX_HTML = filepath.Join(STATIC_DIR, "index.html")
 
-	MAILBOX_URL = os.Getenv("MAILBOX_URL")
+	MAILBOX_URL           = os.Getenv("MAILBOX_URL")
+	AWS_ACCESS_KEY_ID     = os.Getenv("AWS_ACCESS_KEY_ID")
+	AWS_SECRET_ACCESS_KEY = os.Getenv("AWS_SECRET_ACCESS_KEY")
 )
