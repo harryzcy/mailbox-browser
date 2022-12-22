79ab676675b3f449c08129038d1d80ad31a4997f
harryzcy <harry@harryzheng.com>
2022-12-21 23:01:40 -0500

    Fix query and specify response type

--- web/src/services/emails.ts
+++ web/src/services/emails.ts
@@ -1,22 +1,53 @@
+export interface EmailInfo {
+  messageID: string
+  type: 'inbox' | 'draft' | 'sent'
+  timeReceived: string
+  subject: string
+  from: string[]
+  to: string[]
+}
+
 type ListEmailsProps = {
   type: 'inbox' | 'draft' | 'sent'
   year?: number
   month?: number
   order?: 'asc' | 'desc'
+  pageSize?: number
   nextCursor?: string
 }
 
-export async function listEmails(props: ListEmailsProps) {
-  const { type, year, month, order, nextCursor } = props
-  const response = await fetch('/web/emails', {
-    method: 'GET',
-    body: JSON.stringify({
-      type,
-      year,
-      month,
-      order,
-      nextCursor
-    })
+type ListEmailsResponse = {
+  count: number
+  items: EmailInfo[]
+  hasMore: boolean
+  nextCursor?: string
+}
+
+export async function listEmails(
+  props: ListEmailsProps
+): Promise<ListEmailsResponse> {
+  const { type, year, month, order, pageSize, nextCursor } = props
+  const params = new URLSearchParams({
+    type
+  })
+  if (year) {
+    params.append('year', year.toString())
+  }
+  if (month) {
+    params.append('month', month.toString())
+  }
+  if (order) {
+    params.append('order', order)
+  }
+  if (pageSize) {
+    params.append('pageSize', pageSize.toString())
+  }
+  if (nextCursor) {
+    params.append('nextCursor', nextCursor)
+  }
+
+  const response = await fetch('/web/emails?' + params, {
+    method: 'GET'
   })
   return response.json()
 }
