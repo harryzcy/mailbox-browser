6ed6db0ce0ae6facd59dfd3790ab564f907c93dd
Chongyi Zheng <harry@harryzheng.com>
2022-12-25 04:02:51 -0500

    [web] Fix some UI styles in dark and light mode (#35)
    * Fix UI styles in dark and light mode
    
    * Fix prettier check

--- web/src/components/emails/EmailTableRow.tsx
+++ web/src/components/emails/EmailTableRow.tsx
@@ -29,7 +29,7 @@ export default function EmailTableRow(props: EmailTableRowProps) {
     >
       <div
         className={
-          'truncate px-4 py-2 cursor-pointer border-t group-first:border-0 border-gray-900' +
+          'truncate px-4 py-2 cursor-pointer border-t group-first:border-0 border-gray-200 dark:border-gray-900' +
           backgroundClassName
         }
       >
@@ -37,7 +37,7 @@ export default function EmailTableRow(props: EmailTableRowProps) {
       </div>
       <div
         className={
-          'truncate px-4 py-2 cursor-pointer border-t group-first:border-0 border-gray-900' +
+          'truncate px-4 py-2 cursor-pointer border-t group-first:border-0 border-gray-200 dark:border-gray-900' +
           backgroundClassName
         }
       >
@@ -45,7 +45,7 @@ export default function EmailTableRow(props: EmailTableRowProps) {
       </div>
       <div
         className={
-          'px-4 py-2 cursor-pointer border-t group-first:border-0 border-gray-900 text-right' +
+          'px-4 py-2 cursor-pointer border-t group-first:border-0 border-gray-200 dark:border-gray-900 text-right' +
           backgroundClassName
         }
       >
