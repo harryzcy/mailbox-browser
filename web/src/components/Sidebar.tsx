5e95d18740ae405ae2c60cac19598e0a79192542
harryzcy <harry@harryzheng.com>
2022-12-25 05:13:03 -0500

    Add drafts and sent nav links

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -1,6 +1,12 @@
 import { NavLink } from 'react-router-dom'
 
 export default function Sidebar() {
+  const navItems = [
+    ['Inbox', '/inbox'],
+    ['Drafts', '/drafts'],
+    ['Sent', '/sent']
+  ]
+
   return (
     <aside className="sidebar flex-none h-screen md:w-56 text-base border-r dark:border-gray-700">
       <div className="title-group flex flex-col content-center md:p-6">
@@ -9,18 +15,22 @@ export default function Sidebar() {
         </h1>
       </div>
       <nav className="p-3 pt-1">
-        <NavLink
-          to="/inbox"
-          className={({ isActive }) =>
-            `flex content-center px-4 py-2 mb-1 cursor-pointer rounded bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-600 ${
-              isActive
-                ? 'text-gray-800 dark:text-gray-200 dark:bg-gray-700'
-                : ''
-            }`
-          }
-        >
-          Inbox
-        </NavLink>
+        {navItems.map(([name, path]) => {
+          return (
+            <NavLink
+              to={path}
+              className={({ isActive }) =>
+                `flex content-center px-4 py-2 mb-1 cursor-pointer rounded dark:bg-gray-800 dark:hover:bg-gray-600 ${
+                  isActive
+                    ? 'text-gray-900 dark:text-gray-200 bg-gray-300 dark:bg-gray-700'
+                    : 'text-gray-700 dark:text-gray-400 bg-gray-100'
+                }`
+              }
+            >
+              {name}
+            </NavLink>
+          )
+        })}
       </nav>
     </aside>
   )
