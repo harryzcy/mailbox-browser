7a9d2afc5315855a520a73b9c8d5d603235537df
harryzcy <harry@harryzheng.com>
2023-01-03 18:04:55 -0500

    Move info function to a seperate file

--- bff/transport/rest/misc/ping.go
+++ bff/transport/rest/misc/ping.go
@@ -4,22 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	version   = "dev"
-	commit    = "n/a"
-	buildDate = "n/a"
-)
-
 func Ping(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
-	})
-}
-
-func Info(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"version": version,
-		"commit":  commit,
-		"build":   buildDate,
 	})
 }
