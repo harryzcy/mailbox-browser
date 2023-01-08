b00688b841919e3f61ee5c0c8f7b7372e87b3c5d
harryzcy <harry@harryzheng.com>
2023-01-08 18:12:49 -0500

    Support copy and download raw email

--- web/src/pages/EmailRawView.tsx
+++ web/src/pages/EmailRawView.tsx
@@ -3,7 +3,7 @@ import { Await, useLoaderData } from 'react-router-dom'
 
 export default function EmailRawView() {
   const data = useLoaderData() as { messageID: string; raw: string }
-  console.log(data.messageID)
+
   return (
     <div className="w-full px-2 py-2 md:px-8 md:py-5">
       <h1 className="text-lg font-light tracking-wider md:pb-4 md:px-1 dark:text-white">
@@ -17,13 +17,41 @@ export default function EmailRawView() {
           {data.messageID}
         </span>
       </div>
-      <div className="mt-5 p-5 text-sm border rounded-md">
+      <div className="mt-5 p-5 text-sm border rounded-md relative">
         <React.Suspense fallback={<div>Loading...</div>}>
           <Await resolve={data.raw}>
             {(raw) => (
-              <pre className="w-full block whitespace-pre-wrap break-words">
-                {raw}
-              </pre>
+              <>
+                <pre className="w-full block whitespace-pre-wrap break-words">
+                  {raw}
+                </pre>
+
+                <div className="absolute top-2 right-0 p-3 space-x-3">
+                  <span
+                    role="button"
+                    className="bg-blue-100 p-2 rounded-md cursor-pointer"
+                    onClick={() => {
+                      navigator.clipboard.writeText(raw)
+                    }}
+                  >
+                    <span>Copy</span>
+                  </span>
+                  <span
+                    role="button"
+                    className="bg-blue-100 p-2 rounded-md cursor-pointer"
+                    onClick={() => {
+                      const blob = new Blob([raw], { type: 'message/rfc822' })
+                      const url = URL.createObjectURL(blob)
+                      const a = document.createElement('a')
+                      a.href = url
+                      a.download = `${data.messageID}.eml`
+                      a.click()
+                    }}
+                  >
+                    <span>Download</span>
+                  </span>
+                </div>
+              </>
             )}
           </Await>
         </React.Suspense>
