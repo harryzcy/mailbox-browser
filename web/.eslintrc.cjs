2e8fa1a7fbdbda187cc6312b8ae66d5e81efca7f
harryzcy <harry@harryzheng.com>
2023-07-10 19:08:47 -0500

    Fix eslint config

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -5,11 +5,15 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'prettier'
   ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['@typescript-eslint', 'react']
 }
