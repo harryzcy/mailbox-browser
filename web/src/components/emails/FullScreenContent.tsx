6132f54859eed4d0d1b1c4aed7d2f35d1e1e3c73
harryzcy <harry@harryzheng.com>
2023-07-18 21:12:15 -0500

    Save draft via throttled requests

--- web/src/components/emails/FullScreenContent.tsx
+++ web/src/components/emails/FullScreenContent.tsx
@@ -1,10 +1,13 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import {
   DraftEmail,
   DraftEmailsContext
 } from '../../contexts/DraftEmailContext'
 import { deleteEmail, saveEmail } from '../../services/emails'
 import { EmailDraft } from './EmailDraft'
+import useThrottled from '../../hooks/useThrottled'
+
+// const forceUpdateInterval = 5000
 
 interface FullScreenContentProps {
   handleDelete: (messageID: string) => void
@@ -13,7 +16,32 @@ interface FullScreenContentProps {
 export default function FullScreenContent(props: FullScreenContentProps) {
   const draftEmailsContext = useContext(DraftEmailsContext)
 
+  const [draftEmail, setDraftEmail] = useState<DraftEmail | undefined>()
+  const throttledDraftEmail = useThrottled(draftEmail)
+
+  useEffect(() => {
+    const saveDraft = async () => {
+      if (!draftEmail) return
+      await saveEmail({
+        messageID: draftEmail.messageID,
+        subject: draftEmail.subject,
+        from: draftEmail.from,
+        to: draftEmail.to,
+        cc: draftEmail.cc,
+        bcc: draftEmail.bcc,
+        replyTo: draftEmail.from,
+        html: draftEmail.html,
+        text: draftEmail.text,
+        send: false
+      })
+    }
+
+    void saveDraft()
+  }, [throttledDraftEmail])
+
   const handleEmailChange = (email: DraftEmail) => {
+    setDraftEmail(email)
+
     draftEmailsContext.dispatch({
       type: 'update',
       messageID: email.messageID,
@@ -36,30 +64,26 @@ export default function FullScreenContent(props: FullScreenContentProps) {
     })
   }
 
-  const handleSend = () => {
-    const sendRequest = async () => {
-      const email = draftEmailsContext.activeEmail
-      if (!email) return
-      await saveEmail({
-        messageID: email.messageID,
-        subject: email.subject,
-        from: email.from,
-        to: email.to,
-        cc: email.cc,
-        bcc: email.bcc,
-        replyTo: email.from,
-        html: email.html,
-        text: email.text,
-        send: true // save and send
-      })
+  const handleSend = async () => {
+    const email = draftEmailsContext.activeEmail
+    if (!email) return
+    await saveEmail({
+      messageID: email.messageID,
+      subject: email.subject,
+      from: email.from,
+      to: email.to,
+      cc: email.cc,
+      bcc: email.bcc,
+      replyTo: email.from,
+      html: email.html,
+      text: email.text,
+      send: true // save and send
+    })
 
-      draftEmailsContext.dispatch({
-        type: 'remove',
-        messageID: email.messageID
-      })
-    }
-
-    void sendRequest()
+    draftEmailsContext.dispatch({
+      type: 'remove',
+      messageID: email.messageID
+    })
   }
 
   const handleDelete = () => {
@@ -93,7 +117,9 @@ export default function FullScreenContent(props: FullScreenContentProps) {
         handleEmailChange={handleEmailChange}
         handleClose={handleClose}
         handleMinimize={handleMinimize}
-        handleSend={handleSend}
+        handleSend={() => {
+          void handleSend()
+        }}
         handleDelete={handleDelete}
       />
     </div>
