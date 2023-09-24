2731a6849d6e0f0f39a715568aee0a81746ad4f7
Chongyi Zheng <git@zcy.dev>
2023-09-23 21:56:59 -0400

    Handle aws api gateway requests

---
+++ cloudflare/functions/web/[[catchall]].ts
@@ -0,0 +1,26 @@
+import { AwsClient } from 'aws4fetch'
+import { Env } from '../../src/config'
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const segments = context.params.catchall as string[]
+
+  const aws = new AwsClient({
+    accessKeyId: context.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: context.env.AWS_SECRET_ACCESS_KEY,
+    region: context.env.AWS_REGION
+  })
+
+  let endpoint = context.env.AWS_API_GATEWAY_ENDPOINT
+  if (endpoint.endsWith('/')) {
+    endpoint = endpoint.slice(0, -1)
+  }
+  const path = segments.join('/')
+  const url = context.request.url
+  const query = url.includes('?') ? `?${url.split('?')[1]}` : ''
+
+  return aws.fetch(`${endpoint}/${path}${query}`, {
+    method: context.request.method,
+    headers: context.request.headers,
+    body: context.request.body
+  })
+}
