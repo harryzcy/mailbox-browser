4a78dc96f176e6a477636e4ce7b4f5f408ef362e
Chongyi Zheng <git@zcy.dev>
2023-11-25 12:55:02 -0500

    Handle some malformed img tags in email (#674)

---
+++ web/src/utils/elements.ts
@@ -0,0 +1,34 @@
+export const globalAttributes = [
+  'accesskey',
+  'class',
+  'contenteditable',
+  'dir',
+  'draggable',
+  'enterkeyhint',
+  'hidden',
+  'id',
+  'inert',
+  'inputmode',
+  'lang',
+  'popover',
+  'spellcheck',
+  'style',
+  'tabindex',
+  'title',
+  'translate'
+]
+
+export const imgAttributes = [
+  'alt',
+  'crossorigin',
+  'height',
+  'ismap',
+  'loading',
+  'longdesc',
+  'referrerpolicy',
+  'sizes',
+  'src',
+  'srcset',
+  'usemap',
+  'width'
+]
