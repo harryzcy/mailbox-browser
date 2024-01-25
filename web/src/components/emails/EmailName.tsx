d6bc9ce1156d4851ec3aaeccdaae61567e033a79
Chongyi Zheng <git@zcy.dev>
2024-01-25 11:07:47 +0800

    Hide email address in list view (#850)

--- web/src/components/emails/EmailName.tsx
+++ web/src/components/emails/EmailName.tsx
@@ -10,20 +10,18 @@ export default function EmailName({
   const parsed = parseEmailName(emails)
   if (parsed.name === null && parsed.address === null) return null
 
-  if (showAddress) {
-    return parsed.address ?? ''
-  }
-
   if (parsed.name === null) return parsed.address ?? ''
   if (parsed.address === null) return parsed.name
 
   return (
     <>
       <span className="">{parsed.name}</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400">
-        {' '}
-        &lt;{parsed.address}&gt;
-      </span>
+      {showAddress && (
+        <span className="text-sm text-gray-500 dark:text-gray-400">
+          {' '}
+          &lt;{parsed.address}&gt;
+        </span>
+      )}
     </>
   )
 }
