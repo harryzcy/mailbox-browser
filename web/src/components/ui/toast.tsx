f3d7c0d45887d5f8bfaf6207bdd0f99f69242833
Chongyi Zheng <git@zcy.dev>
2024-12-10 03:41:13 -0500

    Update shadcn components to v2.1.7

--- web/src/components/ui/toast.tsx
+++ web/src/components/ui/toast.tsx
@@ -3,7 +3,7 @@ import { type VariantProps, cva } from 'class-variance-authority'
 import { X } from 'lucide-react'
 import * as React from 'react'
 
-import { cn } from 'lib/utils'
+import { cn } from 'components/lib/utils'
 
 const ToastProvider = ToastPrimitives.Provider
 
