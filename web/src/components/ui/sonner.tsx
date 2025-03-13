625770e21e3a642f62cd0a312360618af57be357
Chongyi Zheng <git@zcy.dev>
2025-03-13 01:18:16 -0400

    Use utility class for themes

--- web/src/components/ui/sonner.tsx
+++ web/src/components/ui/sonner.tsx
@@ -11,12 +11,13 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
-          description: 'group-[.toast]:text-muted-foreground',
+            'group toast group-[.toaster]:bg-white group-[.toaster]:text-slate-950 group-[.toaster]:border-slate-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-slate-950 dark:group-[.toaster]:text-slate-50 dark:group-[.toaster]:border-slate-800',
+          description:
+            'group-[.toast]:text-slate-500 dark:group-[.toast]:text-slate-400',
           actionButton:
-            'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground font-medium',
+            'group-[.toast]:bg-slate-900 group-[.toast]:text-slate-50 font-medium dark:group-[.toast]:bg-slate-50 dark:group-[.toast]:text-slate-900',
           cancelButton:
-            'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground font-medium'
+            'group-[.toast]:bg-slate-100 group-[.toast]:text-slate-500 font-medium dark:group-[.toast]:bg-slate-800 dark:group-[.toast]:text-slate-400'
         }
       }}
       {...props}
