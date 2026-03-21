1db9a832d91ca48f046817ba704e1fbcadedbef1
Chongyi Zheng <git@zcy.dev>
2026-03-20 23:41:23 -0500

    Remove vite-tsconfig-paths and use vite to resolve natively

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 const proxyRoutes = ['/web', '/proxy', '/config', '/plugins']
 
@@ -26,10 +25,11 @@ export default defineConfig({
   server: {
     proxy
   },
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@ui': path.resolve(__dirname, './src/components/ui')
-    }
+    },
+    tsconfigPaths: true
   }
 })
