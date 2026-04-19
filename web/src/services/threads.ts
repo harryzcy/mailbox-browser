89af9480d93edef24f05eadc376991d7398cd109
Chongyi Zheng <git@zcy.dev>
2026-04-18 19:03:44 -0500

    Use swr hook to load thread

--- web/src/services/threads.ts
+++ web/src/services/threads.ts
@@ -1,4 +1,6 @@
-import { Email } from './emails'
+import useSWR from 'swr'
+
+import { Email } from 'services/emails'
 
 export interface Thread {
   threadID: string
@@ -9,6 +11,23 @@ export interface Thread {
   timeUpdated: string
   emails: Email[]
   draft?: Email
+}
+
+export function useThread(threadID: string | null) {
+  const { data, error, isLoading } = useSWR<Thread, Error>(
+    threadID ? `thread-${threadID}` : null,
+    async () => {
+      if (!threadID) {
+        throw new Error('Fetching is disabled when threadID is null')
+      }
+      const response = await fetch(`/web/threads/${threadID}`, {
+        method: 'GET'
+      })
+      return response.json() as Promise<Thread>
+    }
+  )
+
+  return { thread: data, error, isLoading }
 }
 
 export async function getThread(threadID: string): Promise<Thread> {
